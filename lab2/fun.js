29ed9e8ed5e68448e6b9cf44662e490ebe66f3b0
Amarjeet Kushwaha <kushwahaamarjeet34@gmail.com>
2026-07-24 15:40:46 +0530

    fun

---
+++ lab2/fun.js
@@ -0,0 +1,17 @@
+const fun1 = () => {
+  console.log("This is fun1");
+};
+const fun2 = () => {
+    console.log("the fun 2");
+};
+const fun3 = () => {
+    console.log("the fun 3");
+};
+function main(){
+    console.log("main");
+    fun1();
+    fun2();
+    fun3();
+    console.log("end");
+}
+main();
