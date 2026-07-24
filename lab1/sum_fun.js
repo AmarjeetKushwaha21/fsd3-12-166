22a08d81c161407e0caf2ba0f539ab9959dad98a
Amarjeet Kushwaha <kushwahaamarjeet34@gmail.com>
2026-07-24 14:38:25 +0530

    eventEmitter

---
+++ lab1/sum_fun.js
@@ -0,0 +1,19 @@
+
+// Write a function to take two numbers and return their sum.
+
+function sum(a,b){
+    return a+b;
+}
+//using arrow funtion
+const add=(a,b)=>{
+    return a+b;
+};
+const add2=(a,b)=>a+b;
+
+console.log("sum : " + sum(20,10));
+console.log("sum : " + add(30,10));
+console.log("sum : " + add2(10,10));
+// frontent- REACT, flask, buejs, angular, 
+// (css) bootsrap, js, tailwind
+
+// Backend-node.js, spring boot, fast api, dzango, PHP, 
